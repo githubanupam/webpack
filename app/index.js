0af2a157281fcf97869b003d12e793c0a7c872da
Anupam Ghosh <mailanupamghosh@gmail.com>
2018-10-27 12:30:32 +0530

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import $ from 'jquery'
+
+var name=require('./test.js')
+
+
+$(function(){
+	$('body').html('<h1>Webpack Test</h1>');
+});
---
+++ app/test.js
@@ -0,0 +1,3 @@
+var name="anupam";
+
+module.exports=name;
